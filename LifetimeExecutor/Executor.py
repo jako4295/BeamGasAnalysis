434bdac0441344425f921116981582566153cf2a
Jakob <jakob@bikeolsen.dk>
2023-08-28 17:43:07 +0200

    Comment in all methods

--- LifetimeExecutor/Executor.py
+++ LifetimeExecutor/Executor.py
@@ -2,9 +2,9 @@ from TestSetups import Tests
 
 
 def main():
-    # Tests.get_lifetimes_from_sigma_estimates()
-    # Tests.get_sigma_estimates_with_varying_i_p()
-    # Tests.get_sigma_estimates_with_varying_n_0()
+    Tests.get_lifetimes_from_sigma_estimates()
+    Tests.get_sigma_estimates_with_varying_i_p()
+    Tests.get_sigma_estimates_with_varying_n_0()
     Tests.get_lifetime_from_data()
 
 
