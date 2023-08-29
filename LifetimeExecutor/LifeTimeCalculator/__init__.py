8d9f7fdcc38aaeb58864732af773c398a7814c6f
Jakob <jakob@bikeolsen.dk>
2023-08-29 11:58:59 +0200

    Made ellipse plot more accessible

--- LifetimeExecutor/LifeTimeCalculator/__init__.py
+++ LifetimeExecutor/LifeTimeCalculator/__init__.py
@@ -2,3 +2,4 @@ from .Calculator import Calculator
 from .ElectronMethods import ElectronMethods, ElectronEnum
 from .Tools import Tools
 from .ResidualGasConstantType import ResidualGasConstantType
+from .RegressionExp import StatisticalSummary
